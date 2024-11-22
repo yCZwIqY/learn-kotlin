71015d559efbc6fd7c53519d65e4ef2ecb1ae348
jylee <jylee@crscube.co.kr>
2024-11-22 16:51:36 +0900

    first commit

---
+++ src/main/kotlin/com/example/learnkotlin/LearnKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learnkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearnKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearnKotlinApplication>(*args)
+}
