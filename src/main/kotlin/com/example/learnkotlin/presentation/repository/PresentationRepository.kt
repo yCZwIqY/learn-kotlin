5833a7b11f89e86c07684559360f097aecc8df8f
jylee <jylee@crscube.co.kr>
2024-11-26 10:20:48 +0900

    리포지토리 개발

--- src/main/kotlin/com/example/learnkotlin/presentation/repository/PresentationRepository.kt
+++ src/main/kotlin/com/example/learnkotlin/presentation/repository/PresentationRepository.kt
@@ -1,6 +1,22 @@
 package com.example.learnkotlin.presentation.repository
 
+import com.example.learnkotlin.domain.entity.*
+import com.example.learnkotlin.domain.repository.*
 import org.springframework.stereotype.Repository
 
 @Repository
-class PresentationRepository
+class PresentationRepository(
+        private val achievementRepository: AchievementRepository,
+        private val introductionRepository: IntroductionRepository,
+        private val linkRepository: LinkRepository,
+        private val skillRepository: SkillRepository,
+        private val projectRepository: ProjectRepository,
+        private val experienceRepository: ExperienceRepository
+) {
+    fun getActiveAchievements(): List<Achievement> = achievementRepository.findAllByIsActive(true)
+    fun getActiveIntroduction(): List<Introduction> = introductionRepository.findAllByIsActive(true)
+    fun getActiveLinks(): List<Link> = linkRepository.findAllByIsActive(true)
+    fun getActiveSkills(): List<Skill> = skillRepository.findAllByIsActive(true)
+    fun getActiveProjects(): List<Project> = projectRepository.findAllByIsActive(true)
+    fun getActiveExperiences(): List<Experience> = experienceRepository.findAllByIsActive(true)
+}
