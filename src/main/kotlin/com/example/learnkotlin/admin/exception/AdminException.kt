65d8f3cb722feea0d2dca81059e2c0e458a86f05
jiyoon <ljy10499@gmail.com>
2024-12-08 18:28:49 +0900

    공통 개발 - Exception과 Advice

--- src/main/kotlin/com/example/learnkotlin/admin/exception/AdminException.kt
+++ src/main/kotlin/com/example/learnkotlin/admin/exception/AdminException.kt
@@ -2,6 +2,17 @@ package com.example.learnkotlin.admin.exception
 
 import org.springframework.http.HttpStatus
 
-class AdminException(httpStatus: HttpStatus, message: String) : RuntimeException(message) {
+abstract class AdminException(httpStatus: HttpStatus, message: String) : RuntimeException(message) {
     val httpStatus: HttpStatus = httpStatus
+
+}
+
+
+class AdminBadRequestException(message: String): AdminException(httpStatus = HttpStatus.BAD_REQUEST, message = message) {
+
+
+}
+
+class AdminInternalServerErrorException(message: String): AdminException(httpStatus = HttpStatus.INTERNAL_SERVER_ERROR, message = message) {
+
 }
